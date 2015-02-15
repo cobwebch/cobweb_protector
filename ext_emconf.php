49c5a672336f400439f51f44ef11de645a71126d
Francois Suter <francois@typo3.org>
2015-02-15 15:35:13 +0100

    Marked as obsolete. Released to TER as version 1.1.1.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "cobweb_protector".
  *
- * Auto generated 15-02-2015 14:00
+ * Auto generated 15-02-2015 15:35
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,13 +15,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Restrict access to a page by checking some value(s), for example coming form a form submission.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'stable',
+	'state' => 'obsolete',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
@@ -34,13 +34,16 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.2.0-4.5.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"a789";s:12:"ext_icon.gif";s:4:"58cc";s:17:"ext_localconf.php";s:4:"790e";s:14:"ext_tables.php";s:4:"fe10";s:13:"locallang.xml";s:4:"7705";s:16:"locallang_db.xml";s:4:"8507";s:10:"README.txt";s:4:"8e1a";s:14:"doc/manual.sxw";s:4:"f02a";s:14:"pi1/ce_wiz.gif";s:4:"82ff";s:36:"pi1/class.tx_cobwebprotector_pi1.php";s:4:"cfba";s:44:"pi1/class.tx_cobwebprotector_pi1_wizicon.php";s:4:"bce2";s:17:"pi1/locallang.xml";s:4:"e890";s:37:"static/Cobweb_Protector/constants.txt";s:4:"d41d";s:33:"static/Cobweb_Protector/setup.txt";s:4:"4c3e";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"a789";s:12:"ext_icon.gif";s:4:"58cc";s:17:"ext_localconf.php";s:4:"790e";s:14:"ext_tables.php";s:4:"fe10";s:13:"locallang.xml";s:4:"7705";s:16:"locallang_db.xml";s:4:"8507";s:9:"README.md";s:4:"0449";s:14:"doc/manual.sxw";s:4:"f02a";s:14:"pi1/ce_wiz.gif";s:4:"82ff";s:36:"pi1/class.tx_cobwebprotector_pi1.php";s:4:"cfba";s:44:"pi1/class.tx_cobwebprotector_pi1_wizicon.php";s:4:"bce2";s:17:"pi1/locallang.xml";s:4:"e890";s:37:"static/Cobweb_Protector/constants.txt";s:4:"d41d";s:33:"static/Cobweb_Protector/setup.txt";s:4:"4c3e";}',
+	'suggests' => array(
+	),
 );
 
 ?>
